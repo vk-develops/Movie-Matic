a27925fce9c4bc620dc9dc461e6a2ab2f8f191f3
vk-develops <vimalkumarv291@gmail.com>
2023-08-13 22:10:26 +0530

    Added responsiveness to the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Movie from "./Components/Movie";
+import Footer from "./Components/Footer";
 
 function App() {
     return (
         <React.Fragment>
             <Movie />
+            <Footer />
         </React.Fragment>
     );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <section className="footer-section">
+            <h5>Designed and Developed by <a href="https://www.instagram.com/itz__vimal__93/"> Vimal Kumar. V</a></h5>
+        </section>
+    );
+}
+
+export default Footer;
